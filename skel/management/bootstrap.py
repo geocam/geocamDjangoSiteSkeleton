5ea46877032c49f85c53f7d2f033998d1cc88a19
trey.smith@nasa.gov <trey.smith@nasa.gov>
2011-03-10 09:17:21 -0800

    now create apps dir if it does not exist (before trying to link submodules)

--- skel/management/bootstrap.py
+++ skel/management/bootstrap.py
@@ -106,6 +106,8 @@ def gitInitSubmodules(opts):
     dosys('git submodule update')
 
 def linkSubmodules(opts):
+    if not os.path.exists('apps'):
+        os.mkdir('apps')
     # assumes each submodule app has a models dir or a models.py file
     submoduleAppDirs = [os.path.dirname(d) for d in glob('submodules/*/*/models*')]
     for src in submoduleAppDirs:
