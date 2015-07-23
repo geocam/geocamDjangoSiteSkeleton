23ed6289061d61b7989a427fc1d98a9365396759
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-07-23 15:43:27 -0700

    moved gumby and fixed manage.py

--- skel/manage.py
+++ skel/manage.py
@@ -18,20 +18,42 @@
 import os
 import sys
 
+# pylint: disable=W0104
+
 # try to bootstrap before hooking into django management stuff
-ret = os.spawnl(os.P_WAIT, sys.executable, sys.executable, '%s/management/bootstrap.py' % os.path.dirname(__file__), '-v')
-if ret != 0:
+if 'bootstrap' in sys.argv:
+    extraArgs = sys.argv[2:]
+else:
+    extraArgs = []
+ret = os.spawnl(os.P_WAIT, sys.executable, sys.executable, '%s/management/bootstrap.py' % os.path.dirname(__file__), *extraArgs)
+if ret != 0 or extraArgs:
     sys.exit(ret)
+
+
+try:
+    from django.conf import settings
+    settings.DATABASES  # force eval
+except:
+    if os.environ.get('SOURCED'):
+        raise
+    else:
+        # may need to source sourceme.sh
+        os.environ['SOURCED'] = '1'
+        thisFile = os.path.realpath(__file__)
+        parentDir = os.path.dirname(thisFile)
+        ret = os.system('bash -c "(source %s/sourceme.sh && %s %s)"'
+                        % (parentDir, thisFile, ' '.join(sys.argv[1:])))
+        sys.exit(int(ret != 0))
+
 
 try:
     import settings  # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<app>.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "$$$$SITE_NAME$$$$.settings")
 
     from django.core.management import execute_from_command_line
 
